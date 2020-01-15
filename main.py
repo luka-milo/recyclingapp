9c1deefbc200e2bc161b9f63202eeb1051c57483
luka-milo <lmilosevic1234@gmail.com>
2020-01-15 13:06:03 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import kivy
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+
+class MyApp(App):
+    def build(self):
+        return Label(text="App")
+
+
+if __name__ == "__main__":
+    MyApp().run()
